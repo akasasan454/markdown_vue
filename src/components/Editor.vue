49960e4380c89caa95a28f51b6cf7d0044ca7af8
大垣内貴文 <ogaito@i-standard2.local>
2019-04-22 21:21:28 +0900

    save

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="editor">
-    <h1>エディター</h1>
+    <h1>エディター画面</h1>
     <span>{{ user.displayName }}</span>
     <button @click="logout">ログアウト</button>
     <div class="editorWrapper">
-      <div class="memoListWrapper"></div>
-      <textarea class="markdown" v-model="markdown"></textarea>
-      <div class="preview" v-html="preview()"></div>
+      <div class="memoListWrapper">
+        <div
+          class="memoList"
+          v-for="(memo, index) in memos"
+          :key="index"
+          @click="selectMemo(index)"
+          :data-selected="index == selectedIndex"
+        >
+          <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
+        </div>
+        <button class="addMemoBtn" @click="addMemo">メモの追加</button>
+        <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
+        <button class="saveMemosBtn" @click="saveMemos">メモの保存</button>
+      </div>
+      <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
+      <div class="preview markdown-body" v-html="preview()"></div>
     </div>
   </div>
 </template>
@@ -18,48 +31,89 @@ export default {
   props: ["user"],
   data() {
     return {
-      memos: [{ markdown: "" }],
+      memos: [
+        {
+          markdown: ""
+        }
+      ],
       selectedIndex: 0
     };
+  },
+  created: function() {
+    firebase
+      .database()
+      .ref("memos/" + this.user.uid)
+      .once("value")
+      .then(result => {
+        if (result.val()) {
+          this.memos = result.val();
+        }
+      });
+  },
+  mounted: function() {
+    document.onkeydown = e => {
+      if (e.key == "s" && (e.metaKey || e.ctrlKey)) {
+        this.saveMemos();
+        return false;
+      }
+    };
+  },
+  beforeDestroy: function() {
+    document.onkeydown = null;
   },
   methods: {
     logout: function() {
       firebase.auth().signOut();
     },
     addMemo: function() {
-      this.memos.push({ markdown: "無題のメモ" });
+      this.memos.push({
+        markdown: "無題のメモ"
+      });
     },
-    selectedMemo: function() {
-      this.slectedIndex = index;
+    deleteMemo: function() {
+      this.memos.splice(this.selectedIndex, 1);
+      if (this.selectedIndex > 0) {
+        this.selectedIndex--;
+      }
+    },
+    saveMemos: function() {
+      firebase
+        .database()
+        .ref("memos/" + this.user.uid)
+        .set(this.memos);
+    },
+    selectMemo: function(index) {
+      this.selectedIndex = index;
     },
     preview: function() {
+      marked.setOptions({ breaks: true });
       return marked(this.memos[this.selectedIndex].markdown);
     },
-    displayName: function(text) {
-      return text.split(/\n/)[0];
+    displayTitle: function(text) {
+      return text.split(/\n/)[0].replace(/#\s/, "");
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.editer {
-  height: 100%;
+.editorWrapper {
+  display: flex;
 }
 .memoListWrapper {
   width: 20%;
-  padding-bottom: 20px;
+  border-top: 1px solid #000;
 }
 .memoList {
   padding: 10px;
   box-sizing: border-box;
   text-align: left;
-  border-bottom: #ccc 1px solid;
+  border-bottom: 1px solid #000;
   &:nth-child(even) {
-    background-color: rgba(#0078ff, 0.1);
+    background-color: #ccc;
   }
   &[data-selected="true"] {
-    background-color: rgba(#00ff30, 0.2);
+    background-color: #ccf;
   }
 }
 .memoTitle {
@@ -68,45 +122,18 @@ export default {
   white-space: nowrap;
   overflow: hidden;
 }
-.memoBtns {
-  padding: 10px;
-  border-bottom: #ccc 1px solid;
-  :nth-child(n + 2) {
-    margin-left: 10px;
-  }
+.addMemoBtn {
+  margin-top: 20px;
 }
 .deleteMemoBtn {
-  background-color: rgba(255, 0, 50, 0.8);
+  margin: 10px;
 }
 .markdown {
-  border: none;
-  border-right: #ccc 1px solid;
-  border-left: #ccc 1px solid;
-  background-color: #eee;
-  box-shadow: inset 0 0 5px 0 rgba(0, 0, 0, 0.1);
-  padding: 10px;
-  width: 50%;
-  resize: none;
-}
-.previewWrapper {
-  text-align: left;
-  width: 30%;
-}
-.previewTitle {
-  color: #888;
-  padding: 10px;
-  font-size: 14px;
-  border-bottom: #ddd 1px dotted;
+  width: 40%;
+  height: 500px;
 }
 .preview {
-  padding: 10px;
-}
-.markdown,
-.memoListWrapper,
-.previewWrapper {
-  overflow: scroll;
-  float: left;
-  height: 100%;
-  box-sizing: border-box;
+  width: 40%;
+  text-align: left;
 }
 </style>
